2c9053d53adf1bd7a1dd6ff09ac7f10a9348cc4c
Kendra Egert <Kendra Egert>
2016-10-31 16:14:33 -0400

    initial commit

---
+++ Chatter/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Chatter.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Messages { get; set; }
+        public ICollection<Message> Entry { get; set; }
+        string Time = Timestamp(DateTime.Now);
+        public static string Timestamp(DateTime value)
+        {
+            return value.ToString("YYMMDDHHmmss");
+        }
+}
+}
